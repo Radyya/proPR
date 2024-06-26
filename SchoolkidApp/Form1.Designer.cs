a04705101acd67583bbfdb88f7d4fb0c45e5da1b
Radyya <gvova3@mail.ru>
2024-06-27 01:51:12 +0300

    Add files via upload

---
+++ SchoolkidApp/Form1.Designer.cs
@@ -0,0 +1,276 @@
+﻿
+namespace SchoolkidApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.dateTimePicker2 = new System.Windows.Forms.DateTimePicker();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 16;
+            this.listBox1.Location = new System.Drawing.Point(16, 15);
+            this.listBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(265, 308);
+            this.listBox1.TabIndex = 0;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(291, 15);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(145, 22);
+            this.textBox1.TabIndex = 1;
+            
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(291, 47);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(4);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(145, 22);
+            this.textBox2.TabIndex = 2;
+            
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(291, 79);
+            this.textBox3.Margin = new System.Windows.Forms.Padding(4);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(145, 22);
+            this.textBox3.TabIndex = 3;
+            
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(291, 111);
+            this.dateTimePicker1.Margin = new System.Windows.Forms.Padding(4);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(145, 22);
+            this.dateTimePicker1.TabIndex = 4;
+           
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(291, 143);
+            this.textBox4.Margin = new System.Windows.Forms.Padding(4);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(145, 22);
+            this.textBox4.TabIndex = 5;
+           
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(291, 175);
+            this.textBox5.Margin = new System.Windows.Forms.Padding(4);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(145, 22);
+            this.textBox5.TabIndex = 6;
+            
+            // 
+            // dateTimePicker2
+            // 
+            this.dateTimePicker2.Location = new System.Drawing.Point(357, 252);
+            this.dateTimePicker2.Margin = new System.Windows.Forms.Padding(4);
+            this.dateTimePicker2.Name = "dateTimePicker2";
+            this.dateTimePicker2.Size = new System.Drawing.Size(145, 22);
+            this.dateTimePicker2.TabIndex = 7;
+            
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(57, 352);
+            this.button1.Margin = new System.Windows.Forms.Padding(4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(100, 28);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "Добавить";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(165, 352);
+            this.button2.Margin = new System.Windows.Forms.Padding(4);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(100, 28);
+            this.button2.TabIndex = 9;
+            this.button2.Text = "Изменить";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(276, 352);
+            this.button3.Margin = new System.Windows.Forms.Padding(4);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(100, 28);
+            this.button3.TabIndex = 10;
+            this.button3.Text = "Удалить";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(443, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(70, 17);
+            this.label1.TabIndex = 11;
+            this.label1.Text = "Фамилия";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(443, 52);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 17);
+            this.label2.TabIndex = 12;
+            this.label2.Text = "Имя";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(443, 82);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(71, 17);
+            this.label3.TabIndex = 13;
+            this.label3.Text = "Отчество";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(443, 111);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(111, 17);
+            this.label4.TabIndex = 14;
+            this.label4.Text = "Дата рождения";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(443, 146);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(133, 17);
+            this.label5.TabIndex = 15;
+            this.label5.Text = "Место проживания";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(443, 178);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(94, 17);
+            this.label6.TabIndex = 16;
+            this.label6.Text = "Место учёбы";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(323, 278);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(222, 17);
+            this.label7.TabIndex = 17;
+            this.label7.Text = "Дата выплаты от департамента";
+            
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(586, 406);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dateTimePicker2);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.listBox1);
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.Text = "Студенты";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        public System.Windows.Forms.ListBox listBox1;
+        public System.Windows.Forms.TextBox textBox1;
+        public System.Windows.Forms.TextBox textBox2;
+        public System.Windows.Forms.TextBox textBox3;
+        public System.Windows.Forms.DateTimePicker dateTimePicker1;
+        public System.Windows.Forms.TextBox textBox4;
+        public System.Windows.Forms.TextBox textBox5;
+        public System.Windows.Forms.DateTimePicker dateTimePicker2;
+        public System.Windows.Forms.Button button1;
+        public System.Windows.Forms.Button button2;
+        public System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+    }
+}
+
---
+++ SchoolkidApp/Form1.cs
@@ -0,0 +1,222 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+
+namespace SchoolkidApp
+{
+
+    public partial class Form1 : Form
+    {
+
+        private List<Student> students;
+
+        public Form1()
+        {
+            InitializeComponent();
+            students = new List<Student>
+
+
+            {
+                new Student
+            {
+                LastName = "Иванов",
+                FirstName = "Иван",
+                MiddleName = "Иванович",
+                DateOfBirth = new DateTime(2008, 3, 15),
+                Residence = "Судак",
+                Workplace = "Школа №2",
+                PaymentDate = new DateTime(2023, 6, 1)
+            },
+        new Student
+        {
+            LastName = "Петров",
+            FirstName = "Петр",
+            MiddleName = "Петрович",
+            DateOfBirth = new DateTime(2007, 7, 20),
+            Residence = "Судак",
+            Workplace = "Школа №3",
+            PaymentDate = new DateTime(2023, 9, 1)
+        },
+        new Student
+        {
+            LastName = "Сидорова",
+            FirstName = "Мария",
+            MiddleName = "Ивановна",
+            DateOfBirth = new DateTime(2006, 11, 12),
+            Residence = "Судак",
+            Workplace = "Гимназия №1",
+            PaymentDate = new DateTime(2023, 4, 15)
+        },
+        new Student
+        {
+            LastName = "Кузнецов",
+            FirstName = "Максим",
+            MiddleName = "Александрович",
+            DateOfBirth = new DateTime(2009, 2, 28),
+            Residence = "Судак",
+            Workplace = "Колледж",
+            PaymentDate = new DateTime(2023, 7, 1)
+        },
+        new Student
+        {
+            LastName = "Смирнова",
+            FirstName = "Анна",
+            MiddleName = "Сергеевна",
+            DateOfBirth = new DateTime(2006, 5, 10),
+            Residence = "Судак",
+            Workplace = "Школа №2",
+            PaymentDate = new DateTime(2023, 11, 1)
+        },
+        new Student
+        {
+            LastName = "Михайлов",
+            FirstName = "Дмитрий",
+            MiddleName = "Михайлович",
+            DateOfBirth = new DateTime(2007, 9, 22),
+            Residence = "Судак",
+            Workplace = "Школа №2",
+            PaymentDate = new DateTime(2023, 2, 15)
+        },
+        new Student
+        {
+            LastName = "Соколова",
+            FirstName = "Елена",
+            MiddleName = "Владимировна",
+            DateOfBirth = new DateTime(2006, 4, 5),
+            Residence = "Судак",
+            Workplace = "Гимназия №4",
+            PaymentDate = new DateTime(2023, 6, 1)
+        },
+        new Student
+        {
+            LastName = "Волков",
+            FirstName = "Андрей",
+            MiddleName = "Александрович",
+            DateOfBirth = new DateTime(2007, 11, 18),
+            Residence = "Судак",
+            Workplace = "Колледж",
+            PaymentDate = new DateTime(2023, 9, 1)
+        },
+        new Student
+        {
+            LastName = "Орлова",
+            FirstName = "Ольга",
+            MiddleName = "Ивановна",
+            DateOfBirth = new DateTime(2006, 8, 3),
+            Residence = "Судак",
+            Workplace = "Школа №4",
+            PaymentDate = new DateTime(2023, 3, 15)
+        },
+        new Student
+        {
+            LastName = "Павлов",
+            FirstName = "Сергей",
+            MiddleName = "Павлович",
+            DateOfBirth = new DateTime(2008, 6, 12),
+            Residence = "Судак",
+            Workplace = "Школа №4",
+            PaymentDate = new DateTime(2023, 12, 1)
+        }
+    };
+            UpdateStudentList();
+        }
+
+
+
+
+        private void UpdateStudentList()
+        {
+            listBox1.Items.Clear();
+            foreach (var student in students)
+            {
+                listBox1.Items.Add(student.FullName);
+            }
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (listBox1.SelectedIndex >= 0)
+            {
+                var selectedStudent = students[listBox1.SelectedIndex];
+                textBox1.Text = selectedStudent.LastName;
+                textBox2.Text = selectedStudent.FirstName;
+                textBox3.Text = selectedStudent.MiddleName;
+                dateTimePicker1.Value = selectedStudent.DateOfBirth;
+                textBox4.Text = selectedStudent.Residence;
+                textBox5.Text = selectedStudent.Workplace;
+                dateTimePicker2.Value = selectedStudent.PaymentDate;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(textBox1.Text) ||
+                string.IsNullOrWhiteSpace(textBox2.Text) ||
+                string.IsNullOrWhiteSpace(textBox3.Text) ||
+                string.IsNullOrWhiteSpace(textBox4.Text) ||
+                string.IsNullOrWhiteSpace(textBox5.Text))
+            {
+                MessageBox.Show("Введите корректный текст в поля", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            var newStudent = new Student
+            {
+                LastName = textBox1.Text,
+                FirstName = textBox2.Text,
+                MiddleName = textBox3.Text,
+                DateOfBirth = dateTimePicker1.Value,
+                Residence = textBox4.Text,
+                Workplace = textBox5.Text,
+                PaymentDate = dateTimePicker2.Value
+            };
+            students.Add(newStudent);
+            UpdateStudentList();
+        }
+
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (listBox1.SelectedIndex >= 0)
+            {
+                var selectedStudent = students[listBox1.SelectedIndex];
+                selectedStudent.LastName = textBox1.Text;
+                selectedStudent.FirstName = textBox2.Text;
+                selectedStudent.MiddleName = textBox3.Text;
+                selectedStudent.DateOfBirth = dateTimePicker1.Value;
+                selectedStudent.Residence = textBox4.Text;
+                selectedStudent.Workplace = textBox5.Text;
+                selectedStudent.PaymentDate = dateTimePicker2.Value;
+                UpdateStudentList();
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (listBox1.SelectedIndex >= 0)
+            {
+                students.RemoveAt(listBox1.SelectedIndex);
+                UpdateStudentList();
+            }
+        }
+
+
+        public class Student
+        {
+
+            public string LastName { get; set; }
+            public string FirstName { get; set; }
+            public string MiddleName { get; set; }
+            public DateTime DateOfBirth { get; set; }
+            public string Residence { get; set; }
+            public string Workplace { get; set; }
+            public DateTime PaymentDate { get; set; }
+
+            public string FullName
+            {
+                get { return $"{LastName} {FirstName} {MiddleName}"; }
+            }
+
+        }
+    }
+}
